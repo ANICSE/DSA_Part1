3ccf08a3b66dc193b92c9d7308c83d1fa5871a8f
AnirbanIITG <anirban2012@iitg.ac.in>
2025-06-22 07:32:14 +0530

    Sorting charatcers by frequency

---
+++ sort_characters.py
@@ -0,0 +1,23 @@
+def sort_character_hash(arr):
+    count = {}
+    arr = arr.lower()
+    print(arr)
+    #arr.lower()
+    for char in arr:
+        #
+        count[char] = count.get(char, 0) + 1
+    
+    sorted_chars = sorted(count.items(), key=lambda x: -x[1])
+
+    result = ''.join([char * count for char, count in sorted_chars])
+
+    return result
+
+def main():
+    print("Sorting charaters using hash")
+    print(sort_character_hash("tree"))
+    print(sort_character_hash("anirban"))
+    print(sort_character_hash("Anirban"))
+
+if __name__ == "__main__":
+    main()
